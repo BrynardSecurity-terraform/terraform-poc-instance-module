f8e17c09dd20544db14887b854529b0e982fde24
RalphBrynard <ralph@thebrynards.com>
2022-04-13 14:36:35 -0400

    Changing output values to account for multiple instances

--- output.tf
+++ output.tf
@@ -7,35 +7,35 @@ output "id" {
 
 output "arn" {
   description = "The ARN of the instance"
-  value       = try(aws_instance.this[0].id, "")
+  value       = try(aws_instance.this.*.id, "")
 }
 
 output "instance_state" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = try(aws_instance.this[0].instance_state, "")
+  value       = try(aws_instance.this.*.instance_state, "")
 }
 
 output "private_dns" {
   description = "The private DNS name assigned to the instance. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       = try(aws_instance.this[0].private_dns, "")
+  value       = try(aws_instance.this.*.private_dns, "")
 }
 
 output "public_dns" {
   description = "The public DNS name assigned to the instance. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = try(aws_instance.this[0].public_dns, "")
+  value       = try(aws_instance.this.*.public_dns, "")
 }
 
 output "public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = try(aws_instance.this[0].public_ip, "")
+  value       = try(aws_instance.this.*.public_ip, "")
 }
 
 output "private_ip" {
   description = "The private IP address assigned to the instance."
-  value       = try(aws_instance.this[0].private_ip, "")
+  value       = try(aws_instance.this.*.private_ip, "")
 }
 
 output "tags_all" {
   description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
-  value       = try(aws_instance.this[0].tags_all, {})
+  value       = try(aws_instance.this.*.tags_all, {})
 }
