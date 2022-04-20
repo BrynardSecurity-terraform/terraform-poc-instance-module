91225491ccdf4415da547ce8b629a2e219ec6332
RalphBrynard <ralph@thebrynards.com>
2022-04-20 11:35:24 -0400

    Removed all provisioners from resource

--- main.tf
+++ main.tf
@@ -12,21 +12,6 @@ resource "aws_instance" "this" {
   subnet_id               = var.subnet_id
   vpc_security_group_ids  = var.vpc_security_group_ids
 
-  provisioner "remote-exec" {
-    inline    = ["echo booted"]
-
-    connection {
-      type      = "winrm"
-       user     = var.remote_exec_user
-       password = var.remote_exec_password
-       host     = self.public_ip
-       port     = 5985
-       insecure = true
-       https    = false
-       timeout  = "10m"
-    }
-  }
-
   tags = merge(
     {"Name" = "${var.staticvmname}" != null ? "${var.staticvmname}" : format("${var.vmname}${var.vmnameformat}", count.index + 1)},
     var.tags
